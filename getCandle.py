01a657fb9755d5226e67727e9c3caced600b40be
unknown <mysm.shahbazi@gmail.com>
2023-03-21 17:35:39 +0330

    refine code for bid and ask

--- getCandle.py
+++ getCandle.py
@@ -1,6 +1,7 @@
 import MetaTrader5 as mt5
 import pandas as pd
 from datetime import datetime
+import datetime as dt
 
 class Rates:
     def __init__(self,symbol,number_of_data,timeframe):
@@ -8,12 +9,21 @@ class Rates:
         self.number_of_data = number_of_data
         self.timeframe = timeframe
         mt5.initialize()
+        self.from_date = datetime.now() # - dt.timedelta(hours=-1)
+        # Extract n Ticks before now
+        ticks = mt5.copy_ticks_from(self.symbol, self.from_date, 10000, mt5.COPY_TICKS_ALL)
+        # Transfrom a tuple into a dataframe
+        df_ticks = pd.DataFrame(ticks)
+        # print(df_ticks)
+        # Convert number format of the date into date format
+        df_ticks["time"] = pd.to_datetime(df_ticks["time"], unit="s")
+        df_ticks['spread'] = df_ticks.ask-df_ticks.bid
+        self.spread = df_ticks['spread'].max()
 
     def get_rates_from_now(self):
         # Compute now date
-        from_date = datetime.now()
         # Extract n Ticks before now
-        rates = mt5.copy_rates_from(self.symbol, self.timeframe, from_date, self.number_of_data)
+        rates = mt5.copy_rates_from(self.symbol, self.timeframe, self.from_date, self.number_of_data)
         mt5.shutdown() 
         # Transform Tuple into a DataFrame
         df_rates = pd.DataFrame(rates)
@@ -21,6 +31,10 @@ class Rates:
         df_rates["time"] = pd.to_datetime(df_rates["time"], unit="s")
     #     df_rates = df_rates.set_index("time")
         return df_rates
+
+    def get_spread(self):
+        return self.spread
+
 
 
 
